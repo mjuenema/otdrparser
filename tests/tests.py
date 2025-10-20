10dd0e643004a1539985496c4a9aee89e433a221
Markus Juenemann <markus@juenemann.net>
2025-10-21 09:42:52 +1100

    Do spellcheck on parse result to prevent issue9 problems in the future

--- tests/tests.py
+++ tests/tests.py
@@ -8,6 +8,7 @@ import otdrparser
 
 ALL_SOR_FILES = glob.glob('*.sor')
 ISSUE9_SOR_FILE = ['issue9.sor']
+ISSUE11_SOR_FILE = ['issue11.sor', 'issue9.sor']
 
 @parameterized.parameterized(ALL_SOR_FILES)
 def test_parsing(sor_file):
@@ -45,5 +46,45 @@ def test_issue9(sor_file):
         for block in blocks:
             if block['name'] == 'DataPts':
                 assert block['data_points'][-1] == (2501.387576623927, -60.749)
+
+
+@parameterized.parameterized(ALL_SOR_FILES)
+def test_issue11(sor_file):
+    """Spellcheck keys in the parsed data structure.
+
+       This is really a one-off check to ensure that there are no
+       other misspelled words in the data structure. It passes
+       for one particular SOR file.
+
+    """
+
+    def recurse(data):
+
+        if isinstance(data, str):
+            if '_' in data:
+                for part in data.split('_'):
+                    recurse(part)
+            elif len(data) > 2:
+                assert data.lower() in words
+        elif isinstance(data, list):
+            for item in data:
+                recurse(item)
+        elif isinstance(data, dict):
+            for key,value in data.items():
+                recurse(key)
+                if not isinstance(value, (str, int)):
+                    recurse(value)
+
+    with open('/usr/share/dict/words', 'rt') as fp:
+        words = [line.strip().lower() for line in fp]
+    words += ['numbytes', 'numblocks', 'genparams', 'supparams','fxdparams','keyevents',
+              'datapts','spclproprietary', 'cksum', 'otdr', 'backscattering',
+              'position2', 'length2', 'points2', 'chksum']
+
+
+    with open(sor_file, 'rb') as fp:
+        blocks = otdrparser.parse(fp)
+
+        recurse(blocks)
 
 
